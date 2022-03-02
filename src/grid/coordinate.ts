fe9abf5e24b1fbc595ab5ad72caf66e15a70008d
Théo FIDRY <theo.fidry@gmail.com>
2022-03-02 11:18:35 +0100

    Revert undesired changes

--- src/grid/coordinate.ts
+++
@@ -1,9 +0,0 @@
-import { Index } from './grid';
-
-export class Coordinate<ColumnIndex extends Index, RowIndex extends Index>{
-    public constructor(
-        public readonly columnIndex: ColumnIndex,
-        public readonly rowIndex: RowIndex,
-    ) {
-    }
-}
--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,7 +1,5 @@
 import { Map } from 'immutable';
 import * as _ from 'lodash';
-import { assertIsNotUndefined } from '../assert/assert-is-not-undefined';
-import { Coordinate } from './coordinate';
 import assert = require('node:assert');
 
 export enum Cell {
@@ -18,36 +16,6 @@ export type GridLines<
     RowIndex extends Index,
 > = Map<RowIndex, Row<ColumnIndex>>;
 
-function getRow<
-    ColumnIndex extends Index,
-    RowIndex extends Index,
->(lines: Readonly<GridLines<ColumnIndex, RowIndex>>, rowIndex: RowIndex): Row<ColumnIndex> {
-    const row = lines.get(rowIndex);
-
-    assertIsNotUndefined(
-        row,
-        OutOfBoundCoordinate.forRow(
-            rowIndex,
-            lines.keySeq().toArray(),
-        ),
-    );
-
-    return row;
-}
-
-function assertRowHasColumn<
-    ColumnIndex extends Index,
-    RowIndex extends Index,
->(row: Readonly<Row<ColumnIndex>>, columnIndex: ColumnIndex): void {
-    assert(
-        row.has(columnIndex),
-        OutOfBoundCoordinate.forColumn(
-            columnIndex,
-            row.keySeq().toArray(),
-        ),
-    );
-}
-
 export class Grid<
     ColumnIndex extends Index,
     RowIndex extends Index,
@@ -58,23 +26,6 @@ export class Grid<
         assertAllRowsHaveSameColumns(lines);
     }
 
-    fillCells(coordinates: Array<Coordinate<ColumnIndex, RowIndex>>): Grid<ColumnIndex, RowIndex> {
-        let lines = this.lines;
-        let row: Row<ColumnIndex>;
-
-        coordinates.forEach(({ columnIndex, rowIndex }) => {
-            row = getRow(lines, rowIndex);
-
-            assertRowHasColumn(row, columnIndex);
-
-            row = row.set(columnIndex, Cell.FULL);
-
-            lines = lines.set(rowIndex, row);
-        });
-
-        return new Grid(lines);
-    }
-
     getLines(): Readonly<GridLines<ColumnIndex, RowIndex>> {
         return this.lines;
     }
@@ -83,7 +34,7 @@ export class Grid<
 function assertAllRowsHaveSameColumns<
     ColumnIndex extends Index,
     RowIndex extends Index,
-    >(lines: Readonly<GridLines<ColumnIndex, RowIndex>>): void {
+>(lines: Readonly<GridLines<ColumnIndex, RowIndex>>): void {
     let columns: ColumnIndex[];
     let rowColumns: ColumnIndex[];
 
@@ -105,36 +56,16 @@ function stringifyIndices<I extends Index>(indices: I[]): string {
     return indices.join('", "');
 }
 
-class InvalidGridError<ColumnIndex extends Index> extends Error {
+class InvalidGridError extends Error {
     constructor(message?: string) {
         super(message);
 
         this.name = 'InvalidGridError';
     }
 
-    static forColumns<ColumnIndex extends Index>(a: ColumnIndex[], b: ColumnIndex[]): InvalidGridError<ColumnIndex> {
+    static forColumns<ColumnIndex extends Index>(a: ColumnIndex[], b: ColumnIndex[]): InvalidGridError {
         return new InvalidGridError(
             `Expected rows to have identical columns. Got "${stringifyIndices(a)}" and "${stringifyIndices(b)}".`,
-        );
-    }
-}
-
-class OutOfBoundCoordinate<ColumnIndex extends Index> extends Error {
-    constructor(message?: string) {
-        super(message);
-
-        this.name = 'InvalidGridError';
-    }
-
-    static forRow<RowIndex extends Index>(rowIndex: RowIndex, rowIndices: RowIndex[]): InvalidGridError<RowIndex> {
-        return new InvalidGridError(
-            `Unknown row index "${rowIndex}". Expected one of "${stringifyIndices(rowIndices)}".`,
-        );
-    }
-
-    static forColumn<ColumnIndex extends Index>(columnIndex: ColumnIndex, columnIndices: ColumnIndex[]): InvalidGridError<ColumnIndex> {
-        return new InvalidGridError(
-            `Unknown column index "${columnIndex}". Expected one of "${stringifyIndices(columnIndices)}".`,
         );
     }
 }
