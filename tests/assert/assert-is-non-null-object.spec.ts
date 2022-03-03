fdf47f0b75840e47c16c6e7ef002f8e23fb7a1ae
Théo FIDRY <theo.fidry@gmail.com>
2022-03-03 01:48:36 +0100

    Review assertions (#20)

---
+++ tests/assert/assert-is-non-null-object.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import { assertIsNonNullObject } from '../../src/assert/assert-is-non-null-object';
+
+describe('assertIsNonNullObject', () => {
+    it('narrows the value type on success', () => {
+        const value: object | null = {};
+
+        assertIsNonNullObject(value);
+
+        // noinspection UnnecessaryLocalVariableJS
+        const objectValue: object = value;
+
+        expect(objectValue).to.equal(value);
+    });
+
+    it('throws upon failure', () => {
+        const value: object | null = null;
+
+        const assert = () => assertIsNonNullObject(value);
+
+        expect(assert).to.throw('false == true');
+    });
+
+    it('throws with a custom message upon failure', () => {
+        const value: object | null = null;
+
+        const assert = () => assertIsNonNullObject(value, 'foo');
+
+        expect(assert).to.throw('foo');
+    });
+
+    it('does not accept undefined', () => {
+        const assert = () => assertIsNonNullObject(undefined);
+
+        expect(assert).to.throw('false == true');
+    });
+});
