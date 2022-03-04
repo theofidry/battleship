b52abbf27dac6901faf906dc10e4bba7bb275a4b
Théo FIDRY <theo.fidry@gmail.com>
2022-03-04 08:12:14 +0100

    Correct coordinates stringability

--- src/grid/coordinate.ts
+++ src/grid/coordinate.ts
@@ -1,9 +1,4 @@
-import { Stringable } from '../utils/stringable';
-
-export class Coordinate<
-    ColumnIndex extends PropertyKey,
-    RowIndex extends PropertyKey,
-> implements Stringable {
+export class Coordinate<ColumnIndex extends PropertyKey, RowIndex extends PropertyKey>{
     public constructor(
         public readonly columnIndex: ColumnIndex,
         public readonly rowIndex: RowIndex,
