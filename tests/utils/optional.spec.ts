15d16fd3822613a7ce6155c3e9e49be5552bcfec
Théo FIDRY <theo.fidry@gmail.com>
2022-03-04 08:04:55 +0100

    Optional improvements (#25)

--- tests/utils/optional.spec.ts
+++ tests/utils/optional.spec.ts
@@ -1,132 +1,166 @@
 import { expect } from 'chai';
-import { just, nothing } from '../../src/utils/optional';
+import { just, nothing, Optional } from '../../src/utils/optional';
 
 describe('Optional', () => {
     const value = 'foo';
 
-    it('can take a value', () => {
-        const optional = just(value);
+    describe('instantiation', () => {
+        it('can take a value', () => {
+            const optional: Optional<string> = just(value);
 
-        expect(optional.isPresent()).to.equal(true);
-        expect(optional.getValue()).to.equal(value);
+            expect(optional.isPresent()).to.equal(true);
+            expect(optional.getValue()).to.equal(value);
+        });
+
+        it('can create an optional which does not contain any value', () => {
+            const optional: Optional<string> = nothing();
+
+            const getValue = () => optional.getValue();
+
+            expect(optional.isPresent()).to.equal(false);
+            expect(getValue).to.throw('No value found.');
+        });
     });
 
-    it('can create an optional which does not contain any value', () => {
-        const optional = nothing();
+    describe('::filter()', () => {
+        it('can filter values (value NOT filtered out)', () => {
+            const optional: Optional<string> = just(value);
+            const filter = (_value: unknown) => 'string' === typeof value;
 
-        const getValue = () => optional.getValue();
+            const filteredOptional: Optional<string> = optional.filter(filter);
 
-        expect(optional.isPresent()).to.equal(false);
-        expect(getValue).to.throw('No value found.');
+            expect(filteredOptional.isPresent()).to.equal(true);
+            expect(filteredOptional.getValue()).to.equal(value);
+            expect(optional.getValue()).to.equal(value);
+        });
+
+        it('can filter values (value filtered out)', () => {
+            const optional: Optional<string> = just(value);
+            const filter = (_value: unknown) => 'number' === typeof value;
+
+            const filteredOptional: Optional<string> = optional.filter(filter);
+
+            expect(filteredOptional.isPresent()).to.equal(false);
+            expect(optional.getValue()).to.equal(value);
+        });
+
+        it('can filter on nothing', () => {
+            const optional: Optional<string> = nothing();
+            const filter = (_value: unknown) => 'number' === typeof value;
+
+            const filteredOptional: Optional<string> = optional.filter(filter);
+
+            expect(filteredOptional.isPresent()).to.equal(false);
+            expect(optional.isPresent()).to.equal(false);
+        });
     });
 
-    it('can filter values (value NOT filtered out)', () => {
-        const optional = just(value);
-        const filter = (_value: unknown) => 'string' === typeof value;
+    describe('::map()', () => {
+        it('can map value', () => {
+            const optional: Optional<string> = just(value);
+            const map = (_value: string) => 10;
 
-        const filteredOptional = optional.filter(filter);
+            const mappedOptional: Optional<number> = optional.map(map);
 
-        expect(filteredOptional.isPresent()).to.equal(true);
-        expect(filteredOptional.getValue()).to.equal(value);
-        expect(optional.getValue()).to.equal(value);
+            expect(mappedOptional.isPresent()).to.equal(true);
+            expect(mappedOptional.getValue()).to.equal(10);
+            expect(optional.getValue()).to.equal(value);
+        });
+
+        it('can map on nothing', () => {
+            const optional: Optional<string> = nothing();
+            const map = (_value: any) => 10;
+
+            const mappedOptional: Optional<number> = optional.map(map);
+
+            expect(mappedOptional.isPresent()).to.equal(false);
+            expect(optional.isPresent()).to.equal(false);
+        });
     });
 
-    it('can filter values (value filtered out)', () => {
-        const optional = just(value);
-        const filter = (_value: unknown) => 'number' === typeof value;
+    describe('::ifPresent()', () => {
+        it('can execute a side-effect', () => {
+            let i = 0;
 
-        const filteredOptional = optional.filter(filter);
+            const optional: Optional<string> = just(value);
+            const sideEffect = (_value: string) => i = _value.length;
 
-        expect(filteredOptional.isPresent()).to.equal(false);
-        expect(optional.getValue()).to.equal(value);
+            const newOptional: Optional<string> = optional.ifPresent(sideEffect);
+
+            expect(i).to.equal(3);
+            expect(optional.isPresent()).to.equal(true);
+            expect(optional.getValue()).to.equal(value);
+            expect(newOptional).to.equal(optional);
+        });
+
+        it('can execute a side-effect on nothing', () => {
+            let i = 0;
+
+            const optional: Optional<string> = nothing();
+            const sideEffect = (_value: any) => i = 10;
+
+            const newOptional: Optional<string> = optional.ifPresent(sideEffect);
+
+            expect(i).to.equal(0);
+            expect(optional.isPresent()).to.equal(false);
+            expect(newOptional).to.equal(optional);
+        });
     });
 
-    it('can filter on nothing', () => {
-        const optional = nothing();
-        const filter = (_value: unknown) => 'number' === typeof value;
+    describe('::orElse()', () => {
+        it('can take a value with default with value present', () => {
+            const optional: Optional<string> = just(value);
 
-        const filteredOptional = optional.filter(filter);
+            const innerValue: number | string = optional.orElse(10);
 
-        expect(filteredOptional.isPresent()).to.equal(false);
-        expect(optional.isPresent()).to.equal(false);
+            expect(innerValue).to.equal(value);
+            expect(optional.isPresent()).to.equal(true);
+            expect(optional.getValue()).to.equal(value);
+        });
+
+        it('can take a value with default with value missing', () => {
+            const optional: Optional<string> = nothing();
+
+            const innerValue: number | string = optional.orElse(10);
+
+            expect(innerValue).to.equal(10);
+            expect(optional.isPresent()).to.equal(false);
+        });
     });
 
-    it('can map value', () => {
-        const optional = just(value);
-        const map = (_value: string) => 10;
+    describe('inner optional', () => {
+        it('can take another optional value', () => {
+            const optional: Optional<Optional<string>> = just(just(value));
 
-        const mappedOptional = optional.map(map);
+            expect(optional.isPresent()).to.equal(true);
+            expect(optional.getValue().getValue()).to.equal(value);
+        });
 
-        expect(mappedOptional.isPresent()).to.equal(true);
-        expect(mappedOptional.getValue()).to.equal(10);
-        expect(optional.getValue()).to.equal(value);
+        it('can take another nothing value', () => {
+            const optional: Optional<Optional<string>> = just(nothing());
+
+            expect(optional.isPresent()).to.equal(true);
+            expect(optional.getValue().isPresent()).to.equal(false);
+        });
     });
 
-    it('can map on nothing', () => {
-        const optional = nothing();
-        const map = (_value: any) => 10;
+    describe('::orElseThrow()', () => {
+        it('does not throw if there is a value', () => {
+            const optional: Optional<string> = just(value);
+            const error = new Error();
 
-        const mappedOptional = optional.map(map);
+            const elseThrow: string = optional.orElseThrow(error);
 
-        expect(mappedOptional.isPresent()).to.equal(false);
-        expect(optional.isPresent()).to.equal(false);
-    });
+            expect(elseThrow).to.not.throw;
+        });
 
-    it('can execute a side-effect', () => {
-        let i = 0;
+        it('throws if there is no value', () => {
+            const optional: Optional<string> = nothing();
+            const error = new Error();
 
-        const optional = just(value);
-        const sideEffect = (_value: string) => i = _value.length;
+            const elseThrow: ()=> string = () => optional.orElseThrow(error);
 
-        optional.ifPresent(sideEffect);
-
-        expect(i).to.equal(3);
-        expect(optional.isPresent()).to.equal(true);
-        expect(optional.getValue()).to.equal(value);
-    });
-
-    it('can execute a side-effect on nothing', () => {
-        let i = 0;
-
-        const optional = nothing();
-        const sideEffect = (_value: any) => i = 10;
-
-        optional.ifPresent(sideEffect);
-
-        expect(i).to.equal(0);
-        expect(optional.isPresent()).to.equal(false);
-    });
-
-    it('can take a value with default with value present', () => {
-        const optional = just(value);
-
-        const innerValue = optional.orElse(10);
-
-        expect(innerValue).to.equal(value);
-        expect(optional.isPresent()).to.equal(true);
-        expect(optional.getValue()).to.equal(value);
-    });
-
-    it('can take a value with default with value missing', () => {
-        const optional = nothing();
-
-        const innerValue = optional.orElse(10);
-
-        expect(innerValue).to.equal(10);
-        expect(optional.isPresent()).to.equal(false);
-    });
-
-    it('can take another optional value', () => {
-        const optional = just(just(value));
-
-        expect(optional.isPresent()).to.equal(true);
-        expect(optional.getValue().getValue()).to.equal(value);
-    });
-
-    it('can take another nothing value', () => {
-        const optional = just(nothing());
-
-        expect(optional.isPresent()).to.equal(true);
-        expect(optional.getValue().isPresent()).to.equal(false);
+            expect(elseThrow).to.throw(error);
+        });
     });
 });
