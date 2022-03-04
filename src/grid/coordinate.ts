bb8566da410b5eaee7aeb25ebfd2022dcb51af52
Théo FIDRY <theo.fidry@gmail.com>
2022-03-04 08:10:03 +0100

    Introduce Stringable interface

--- src/grid/coordinate.ts
+++ src/grid/coordinate.ts
@@ -1,7 +1,16 @@
-export class Coordinate<ColumnIndex extends PropertyKey, RowIndex extends PropertyKey>{
+import { Stringable } from '../utils/stringable';
+
+export class Coordinate<
+    ColumnIndex extends PropertyKey,
+    RowIndex extends PropertyKey,
+> implements Stringable {
     public constructor(
         public readonly columnIndex: ColumnIndex,
         public readonly rowIndex: RowIndex,
     ) {
+    }
+
+    toString(): string {
+        return this.columnIndex.toString() + this.rowIndex.toString();
     }
 }
