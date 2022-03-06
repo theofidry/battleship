ac1d29103f34adc1d89224182fcce3d064e4659f
Théo FIDRY <theo.fidry@gmail.com>
2022-03-06 18:52:59 +0100

    Add PositionedShip (#34)

---
+++ src/ship/positioned-ship.ts
@@ -0,0 +1,75 @@
+import { List, OrderedSet } from 'immutable';
+import { Coordinate } from '../grid/coordinate';
+import { Ship } from './ship';
+import assert = require('node:assert');
+
+export class PositionedShip<
+    ColumnIndex extends PropertyKey,
+    RowIndex extends PropertyKey,
+> {
+    private readonly hitCoordinates: Map<string, boolean>;
+    private sunk = false;
+
+    constructor(
+        public readonly ship: Ship,
+        public readonly coordinates: OrderedSet<Coordinate<ColumnIndex, RowIndex>>,
+    ) {
+        // Sanity check
+        assert(ship.size === coordinates.size);
+
+        this.hitCoordinates = new Map(
+            coordinates.map((coordinate) => [coordinate.toString(), false]),
+        );
+    }
+
+    markAsHit(coordinate: Coordinate<ColumnIndex, RowIndex>): void {
+        const coordinateKey = coordinate.toString();
+        this.assertIsKnownCoordinate(coordinateKey);
+
+        this.hitCoordinates.set(coordinateKey, true);
+
+        this.sunk = calculateIsSunk(this.getHits().toArray());
+    }
+
+    isSunk(): boolean {
+        return this.sunk;
+    }
+
+    getHits(): List<boolean> {
+        return List(this.hitCoordinates.values());
+    }
+
+    private assertIsKnownCoordinate(coordinate: string): void {
+        assert(
+            this.hitCoordinates.has(coordinate),
+            OutOfBoundShipCoordinate.forCoordinates(
+                coordinate,
+                Array.from(this.hitCoordinates.keys()),
+            ),
+        );
+    }
+}
+
+class OutOfBoundShipCoordinate extends Error {
+    constructor(message?: string) {
+        super(message);
+
+        this.name = 'OutOfBoundShipCoordinate';
+    }
+
+    static forCoordinates(
+        invalidCoordinate: string,
+        knownCoordinates: ReadonlyArray<string>,
+    ): OutOfBoundShipCoordinate {
+        return new OutOfBoundShipCoordinate(
+            `Unknown coordinate "${invalidCoordinate.toString()}". Expected one of "${knownCoordinates.join('", "')}".`,
+        );
+    }
+}
+
+function calculateIsSunk(hits: boolean[]): boolean {
+    return hits.reduce(
+        (sunk: boolean, hit) => sunk && hit,
+        true,
+    );
+}
