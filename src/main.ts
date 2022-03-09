52220424a760da219586d535e648b1f30d085acf
Théo FIDRY <theo.fidry@gmail.com>
2022-03-09 23:29:59 +0100

    Allow user to enter the coordinate again (#52)

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import { createInteractivePlayer } from './standard-grid/interactive-player-fact
 import { STD_COLUMN_INDICES } from './standard-grid/std-column-index';
 import { STD_ROW_INDICES } from './standard-grid/std-row-index';
 
-const match = new Match(new ConsoleLogger());
+const logger = new ConsoleLogger();
+const match = new Match(logger);
 const fleet = createFleet();
 
 match
     .play(
         createDumbPlayer('A', fleet),
-        createInteractivePlayer(fleet),
+        createInteractivePlayer(fleet, logger),
         STD_COLUMN_INDICES.length * STD_ROW_INDICES.length * 2,
     )
     .subscribe();
