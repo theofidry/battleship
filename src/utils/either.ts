ec910ec5959faec64435e52ec502b30fb713b54a
Théo FIDRY <theo.fidry@gmail.com>
2022-03-11 12:34:09 +0100

    Add Either (#60)

---
+++ src/utils/either.ts
@@ -0,0 +1,118 @@
+// In order to get all the nice type safety of type guards when switching on a
+// union type, that union type needs to have a "discriminant property", which
+// has a concrete and unique value defined for each of its composite types.
+import { isNonNullObject } from '../assert/assert-is-non-null-object';
+import { hasOwnProperty } from './has-own-property';
+import { just, nothing, Optional } from './optional';
+
+const LeftTypeDiscriminant = Symbol('left_type');
+const RightTypeDiscriminant = Symbol('right_type');
+
+type Left<L> = {
+    readonly type: typeof LeftTypeDiscriminant;
+    readonly value: L;
+};
+
+function isLeft<T>(value: unknown): value is Left<T> {
+    return isNonNullObject(value)
+        && hasOwnProperty(value, 'type')
+        && LeftTypeDiscriminant === value['type'];
+}
+
+function leftObject<T>(value: T): Left<T> {
+    return {
+        type: LeftTypeDiscriminant,
+        value,
+    };
+}
+
+type Right<R> = {
+    readonly type: typeof RightTypeDiscriminant;
+    readonly value: R;
+};
+
+function isRight<R>(value: unknown): value is Right<R> {
+    return isNonNullObject(value)
+        && hasOwnProperty(value, 'type')
+        && RightTypeDiscriminant === value['type'];
+}
+
+function rightObject<R>(value: R): Right<R> {
+    return {
+        type: RightTypeDiscriminant,
+        value,
+    };
+}
+
+/**
+ * Either represents a value of two possible types and is right biased.
+ */
+export class Either<L, R> {
+    private constructor(
+        private readonly value: Left<L> | Right<R>,
+    ) {}
+
+    fold<T>(
+        onLeft: (left: L)=> T,
+        onRight: (right: R)=> T,
+    ): T {
+        const sidedValue = this.value;
+
+        return isLeft(sidedValue)
+            ? onLeft(sidedValue.value)
+            : onRight(sidedValue.value);
+    }
+
+    map<T>(mapper: (right: R)=> T): Either<L, T> {
+        return this.flatMap(
+            (_right) => Either.right(mapper(_right)),
+        );
+    }
+
+    flatMap<T>(mapper: (right: R)=> Either<L, T>): Either<L, T> {
+        return this.fold(
+            (leftValue) => Either.left(leftValue),
+            (rightValue) => mapper(rightValue),
+        );
+    }
+
+    swap(): Either<R, L> {
+        return this.fold(
+            (leftValue) => Either.right(leftValue),
+            (rightValue) => Either.left(rightValue),
+        );
+    }
+
+    getOrElse(defaultValue: R): R {
+        return this.fold(
+            () => defaultValue,
+            (value) => value,
+        );
+    }
+
+    getOrThrow(error: Error): R {
+        const throwError = () => {
+            throw error;
+        };
+
+        return this.fold(
+            () => throwError(),
+            (rightValue) => rightValue,
+        );
+    }
+    
+    toOptional(): Optional<R> {
+        return this.fold(
+            () => nothing(),
+            (rightValue) => just(rightValue),
+        );
+    }
+
+    static left<L, R>(value: L): Either<L, R> {
+        return new Either<L, R>(leftObject(value));
+    }
+
+    static right<L, R>(value: R): Either<L, R> {
+        return new Either<L, R>(rightObject(value));
+    }
+}
---
+++ tests/utils/either.spec.ts
@@ -0,0 +1,72 @@
+import { expect } from 'chai';
+import { Either } from '../../src/utils/either';
+import { just, nothing } from '../../src/utils/optional';
+
+describe('Either', () => {
+    it('::fold() flattens the value', () => {
+        const eitherLeft: Either<string, number> = Either.left('foo');
+        const eitherRight: Either<string, number> = Either.right(10);
+
+        const fold = (either: Either<string, number>) => either.fold(
+            (value: string) => value.toUpperCase(),
+            (value: number) => String(-value),
+        );
+
+        expect(fold(eitherLeft)).to.equal('FOO');
+        expect(fold(eitherRight)).to.equal('-10');
+    });
+
+    it('::map() maps the right value and leave the left unchanged', () => {
+        const eitherLeft: Either<number, number> = Either.left(3);
+        const eitherRight: Either<number, number> = Either.right(7);
+
+        const mapper = (value: number) => value * 2;
+
+        expect(eitherLeft.map(mapper)).to.eqls(eitherLeft);
+        expect(eitherRight.map(mapper)).to.eqls(Either.right(14));
+    });
+
+    it('::flatMap() maps the right value and leave the left unchanged', () => {
+        const eitherLeft: Either<number, number> = Either.left(3);
+        const eitherRight: Either<number, number> = Either.right(7);
+
+        const mapper = (value: number): Either<number, number> => Either.right(value * 2);
+
+        expect(eitherLeft.flatMap(mapper)).to.eqls(eitherLeft);
+        expect(eitherRight.flatMap(mapper)).to.eqls(Either.right(14));
+    });
+
+    it('::swap() makes a left into a right and a right into a left', () => {
+        const eitherLeft: Either<number, number> = Either.left(3);
+        const eitherRight: Either<number, number> = Either.right(7);
+
+        expect(eitherLeft.swap()).to.eqls(Either.right(3));
+        expect(eitherRight.swap()).to.eqls(Either.left(7));
+    });
+
+    it('::toOptional() returns an optional of the right value', () => {
+        const eitherLeft: Either<number, number> = Either.left(3);
+        const eitherRight: Either<number, number> = Either.right(7);
+
+        expect(eitherLeft.toOptional()).to.eqls(nothing());
+        expect(eitherRight.toOptional()).to.eqls(just(7));
+    });
+
+    it('::getOrElse() gets the right value otherwise picks the default value (for the left)', () => {
+        const eitherLeft: Either<number, number> = Either.left(3);
+        const eitherRight: Either<number, number> = Either.right(7);
+
+        expect(eitherLeft.getOrElse(-1)).to.equal(-1);
+        expect(eitherRight.getOrElse(-1)).to.equal(7);
+    });
+
+    it('::getOrThrow() gets the right value otherwise throw the error (for the left)', () => {
+        const eitherLeft: Either<number, number> = Either.left(3);
+        const eitherRight: Either<number, number> = Either.right(7);
+
+        const error = new Error('foo');
+
+        expect(() => eitherLeft.getOrThrow(error)).to.throw(error);
+        expect(eitherRight.getOrThrow(error)).to.equal(7);
+    });
+});
