7174cf677a86bbc8215c10fd15bc6f347ea194c2
Théo FIDRY <theo.fidry@gmail.com>
2022-03-15 11:14:00 +0100

    Remove redundant check (#74)

--- src/assert/assert-is-not-undefined.ts
+++ src/assert/assert-is-not-undefined.ts
@@ -5,6 +5,5 @@ export function isNotUndefined<T>(value: T | undefined): value is T {
 }
 
 export function assertIsNotUndefined<T>(value: T | undefined, message?: Error | string): asserts value is T {
-    assert(value !== undefined, message);
     assert(isNotUndefined(value), message);
 }
