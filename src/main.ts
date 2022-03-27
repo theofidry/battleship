4c809a7f017186845b79658588607cf1855d32ab
Théo FIDRY <theo.fidry@gmail.com>
2022-03-27 14:28:15 +0200

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,5 @@
-import { Command } from 'commander';
-import { AIMatchCommand } from './command/ai-match-command';
 import { mainCommand } from './command/application';
-import { createDebugOption } from './command/debug-option';
 
-AIMatchCommand.parseAsync();
-//
-//
-// const command = new Command('test');
-// command.addOption(createDebugOption());
-// command.action(() => {
-//     console.log({ ops: command.opts() });
-//
-//     return new Promise(() => true);
-// });
-// command.parseAsync(process.argv)
-//     .then(() => process.exit(0))
-//     .catch((error) => mainCommand.error(error));
-//
-// mainCommand.parseAsync(process.argv)
-//     .then(() => process.exit(0))
-//     .catch((error) => mainCommand.error(error));
+mainCommand.parseAsync(process.argv)
+    .then(() => process.exit(0))
+    .catch((error) => mainCommand.error(error));
